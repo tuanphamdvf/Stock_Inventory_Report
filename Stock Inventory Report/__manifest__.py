9f8a881bce355b858184f17af41723b9012526ac
tuanphamdvf <104239862+tuanphamdvf@users.noreply.github.com>
2023-04-29 21:35:34 +0700

    [update]

--- Stock Inventory Report/__manifest__.py
+++ Stock Inventory Report/__manifest__.py
@@ -17,7 +17,7 @@
     'category': 'App',
     'version': '15.1.1',
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'tree_view', 'product', 'purchase', 'mrp'],
+    'depends': ['base', 'stock', 'product', 'purchase', 'mrp'],
 
     # always loaded
     'data': [
